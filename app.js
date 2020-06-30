7c856599f9df0e62febd3326b7325c12cf1575ef
Akarsh Kashamshetty <akarshkashamshetty@Akarshs-MacBook-Pro.local>
2020-06-30 00:59:31 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+//my routes
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+
+//DBConnections
+mongoose.connect(process.env.DATABASE,{
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+    useCreateIndex : true
+})
+
+.then(()=>{
+    console.log("DB CONNECTED");
+});
+
+//Middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+//myroutes
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+
+
+//port
+const port = process.env.PORT || 8000;
+
+
+//starting a server
+app.listen(port, ()=>{
+    console.log(`app is running at ${port}`)
+});
