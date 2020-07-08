f4741f91f64a277efcc4dae6e7be058c473aadb5
Akarsh Kashamshetty <akarsh9076@gmail.com>
2020-07-07 23:42:29 -0400

    order routes

---
+++ routes/order.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const router = express.Router()
+
+const {isSignedIn, isAdmin, isAuthenticated} = require("../controllers/auth")
+const {getUserById, pushOrderInPurchaseList} = require("../controllers/user")
+const {updateStock} = require("../controllers/product")
+
+
+const {} = require("../controllers/order")
